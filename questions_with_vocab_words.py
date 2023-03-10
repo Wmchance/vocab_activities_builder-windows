105914e8b9843388ecff9916f47bfe91969ac2ad
Wmchance <wmchance@gmail.com>
2023-03-10 12:04:29 +0700

    update instructions and word bank

--- questions_with_vocab_words.py
+++ questions_with_vocab_words.py
@@ -7,7 +7,9 @@ def questions_with_vocab_words(OPENAI_API_KEY, word_str, destination_path):
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"Key words and phrases: {word_str}. \nCreate a horizontal word list for the above key words. Write 'Word Bank:' above the word list.\n\nFor each word in the word list, write two questions using that word.\nThe questions should be at the CEFR A2 English level.",
+        prompt=f'''Key words and phrases: {word_str}. 
+        \nFor each word in the word list, write two questions using that word.
+        \nThe questions should be at the CEFR A2 English level.''',
         temperature=0.7,
         max_tokens=2061,
         top_p=1,
@@ -17,12 +19,14 @@ def questions_with_vocab_words(OPENAI_API_KEY, word_str, destination_path):
     
     response_text = response.choices[0]['text']
 
-    # add spaces blanks to text manually so that ChatGPT is relied on for this part
-    # add a word bank
-
     with open(destination_path, 'a') as fp:
         pass
         fp.write("Questions with Vocab Words\n")
+        fp.write(" \n")
+        fp.write("Word Bank:\n")
+        fp.write(f'{word_str}\n')
+        fp.write(" \n")
+        fp.write("Instructions: Answer each of the below questions. There are two for each word.\n")
         fp.write(" \n")
         fp.write(f'{response_text}\n')
         fp.write(' \n')
